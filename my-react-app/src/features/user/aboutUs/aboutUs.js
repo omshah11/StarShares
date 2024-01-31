4ced2b4c62ee31f5821b72f2df9bd3df0b10c0f1
omshah11 <61638057+omshah11@users.noreply.github.com>
2024-01-30 23:55:45 -0800

    Edited the landingpage frontend design
    Added routing and basic frontend design for the About Us page from the Navbar.

---
+++ my-react-app/src/features/user/aboutUs/aboutUs.js
@@ -0,0 +1,18 @@
+// Import necessary dependencies
+import React from 'react';
+import "./aboutUs.css"
+
+// About Us page component
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <h2>Our Goal</h2>
+      <p>This is the About Us page. Add your description here.</p>
+      <h2>Who We Are</h2>
+      <p>This is the About Us page. Add your description here.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
