480e2cff52add2a14d29493b6f61f32da4e90c86
ricericericeee <patricecalagui@gmail.com>
2024-02-17 14:48:15 -0800

    Update searchbar.js

--- my-react-app/src/features/user/search/searchbar.js
+++ my-react-app/src/features/user/search/searchbar.js
@@ -73,14 +73,7 @@ function Search() {
             }}
             onChange={event => setSearchInput(event.target.value)}
           />
-          {/* <Button onClick={search}>
-            Search
-          </Button> */}
-          {/* <button class="btn" id="btn">
-            Search
-            <i class="bx bx-chevron-down" id="arrow"></i>
-          </button> */}
-          
+
           <div>
             <button onClick={handleButtonClick}>Search</button>
             {buttonClicked && (
@@ -96,48 +89,9 @@ function Search() {
           </div>
         </InputGroup>
       </Container>
-      
-      {/* <Container>
-        <Row className='mx-2 row row-cols-4'>
-          {albums.map((album, i) => {
-            console.log(album);
-            return(
-              <Card>
-                <Card.Img src={album.images[2].url}/>
-                <Card.Body>
-                  <Card.Title>{album.name}</Card.Title>
-                </Card.Body>
-              </Card>
-            )
-          })}
-        </Row>
-      </Container> */}
 
     </div>
   );
 }
-
-// const dropdownBtn = document.getElementById("btn");
-// const dropdownMenu = document.getElementById("dropdown");
-// const toggleArrow = document.getElementById("arrow");
-
-// // Toggle dropdown function
-// const toggleDropdown = function () {
-//   dropdownMenu.classList.toggle("show");
-//   toggleArrow.classList.toggle("arrow");
-// };
-
-// // Toggle dropdown open/close when dropdown button is clicked
-// dropdownBtn.addEventListener("click", function (e) {
-//   e.stopPropagation();
-//   toggleDropdown();
-// });
-
-// // Close dropdown when dom element is clicked
-// document.documentElement.addEventListener("click", function () {
-//   if (dropdownMenu.classList.contains("show")) {
-//     toggleDropdown();
-//   }
-// });
 
 export default Search;
