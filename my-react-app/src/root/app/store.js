18b1730bc2792e44ae24f3e3772f8624d2582548
omshah11 <61638057+omshah11@users.noreply.github.com>
2024-02-17 21:10:52 -0800

    Added comments

--- my-react-app/src/root/app/store.js
+++ my-react-app/src/root/app/store.js
@@ -1,10 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../../features/counter/counterSlice'
-import userReducer from '../../features/user/userSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '../../features/counter/counterSlice'; // Importing the counter slice reducer
+import userReducer from '../../features/user/userSlice'; // Importing the user slice reducer
 
+// Configure the Redux store with reducers for managing state
 export default configureStore({
   reducer: {
-      counter: counterReducer,
-      user: userReducer,
+      counter: counterReducer, // Assigning the counter slice reducer under the 'counter' key
+      user: userReducer, // Assigning the user slice reducer under the 'user' key
   }
-})
+});
