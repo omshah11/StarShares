98ba31c667fe211944e236ca9b138405f297051c
ricericericeee <patricecalagui@gmail.com>
2024-03-09 10:12:06 -0800

    Update searchPage.js

--- my-react-app/src/features/user/search/searchPage.js
+++ my-react-app/src/features/user/search/searchPage.js
@@ -1,7 +1,7 @@
 // Import necessary dependencies
 // import React from 'react';
 import "./searchPage.css"
-import {Container, InputGroup, FormControl, Button, Row, Card} from 'react-bootstrap';
+import {Container, InputGroup, FormControl, Button, Row, Col, Card, Modal} from 'react-bootstrap';
 import {useState, useEffect, React, useContext} from 'react';
 import {useLocation} from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -43,37 +43,76 @@ const SearchPage = () => {
         'Authorization': 'Bearer ' + accessToken
       }
     }
-    let artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=album,artist,track', searchParameters)
+    let artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=album,artist', searchParameters)
       .then(response => response.json())
       .then(data => {
         console.log(data);
         setItems(data.artists.items);
         setItems(data.albums.items);
-        setItems(data.tracks.items);
+        //setItems(data.tracks.items);
       });
 
     //console.log("Artist ID is " + artistID)
   }
+
+  const openModal = () => {
+    var modalWrap = document.createElement('div');
+    modalWrap.innerHTML = `
+      <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  };
 
   // console.log(process.env.CLIENT_ID)
   // console.log(process.env.CLIENT_SECRET)
   search();
   return (
     <div>
-      <Container>
-      <Row className='mx-2 row row-cols-5'>
+      {/* <Container> */}
+      {/* <Row className='mx-2 row row-cols-5'> */}
+      {/* <Row> */}
+        {/* <Col className='col-5 col-md-6'> */}
       {items.map((item, i) => {
           console.log(item);
           return(
-            <Card>
-              <Card.Body>
-                <Card.Title>{item.name}</Card.Title>
-              </Card.Body>
-            </Card>
+            // <Card>
+            //   {/* <Card.Img scr={item.images[0]}/> */}
+            //   <Card.Body>
+            //     <Card.Title>{item.name}</Card.Title>
+            //   </Card.Body>
+            // </Card>
+            <div>
+              <Container>
+                <Row className='mx-2 row row-cols-5'>
+                  <Button onClick={openModal}>
+                    {item.name}
+                  </Button>
+                </Row>
+              </Container>
+              
+            </div>
           )
         })}
-      </Row>
-    </Container>
+        {/* </Col> */}
+      {/* </Row> */}
+    {/* </Container> */}
   </div>
   );
 };
