4e45bd933cd179273c761a7e61709bde527de2f1
ricericericeee <patricecalagui@gmail.com>
2024-04-22 23:19:20 -0700

    Update searchPage.js

--- my-react-app/src/features/user/search/searchPage.js
+++ my-react-app/src/features/user/search/searchPage.js
@@ -207,7 +207,18 @@ const SearchPage = () => {
                       </div>
                       <div className="right">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-                        <Button className="stockBtn">Artist Page</Button>
+                        {/* <p>{selectedCard.artists.map(artist => artist.name).join(', ')}</p> */}
+                        {/* <Button className="stockBtn">Artist Page</Button> */}
+                        {selectedCard.artists.map((artist) => (
+                        <Button
+                          key={artist.id}
+                          variant="outline-primary"
+                          className="stockBtn"
+                          // onClick={() => handleArtistClick(artist)}
+                        >
+                          {artist.name}
+                        </Button>
+                      ))}
                       </div>
                     </>
                   ) : (
