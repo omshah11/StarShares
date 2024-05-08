f8cfb3edad8c9297d3ca8456cb86ca72ff08bea7
KanayoAnyakpor <119722153+KanayoAnyakpor@users.noreply.github.com>
2024-05-08 00:26:30 -0700

    error handling

--- my-react-app/src/features/artist/BuyModal.js
+++ my-react-app/src/features/artist/BuyModal.js
@@ -2,92 +2,97 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const BuyModal = ({ showModal, closeModal, userId, stockId, artistImage, artistName, spotifyId }) => {
-    const [quantity, setQuantity] = useState(0);
+  const [quantity, setQuantity] = useState(0);
 
-    const handleBuy = async (userId, stockId, artistImage, artistName, quantity, spotifyId) => {
-      let newStockId;
-  
-      try {
-          // Attempt to add the stock
-          const addStock = await axios.post(
-              "http://localhost:5000/api/addStock",
-              {
-                  artistName: artistName,
-                  artistImage: artistImage,
-                  spotifyId: spotifyId,
+  const handleBuy = async (userId, stockId, artistImage, artistName, quantity, spotifyId) => {
+    let newStockId;
 
-                  // You can define cost to be a call to the algorithm
-              }
-          );
-  
-          // Log the result of adding the stock
-          console.log("Stock added to DB:", addStock.data);
-  
-          // Extract the newly added stock ID from the response
-          newStockId = addStock.data.stock?._id;
-  
-          if (!newStockId) {
-              throw new Error("Failed to retrieve newly added stock ID");
-          }
-  
-          // Update stockId with newStockId
-          stockId = newStockId;
-      } catch (error) {
-          // Log the error if adding the stock fails
-          console.error("Error adding stock:", error);
-      }
-  
-      try {
-          // Add the newly added stock to the user's portfolio
-          const addToPortfolio = await axios.post(
-              "http://localhost:5000/api/addStockToPortfolio",
-              {
-                  userId: userId,
-                  stockId: stockId,
-                  quantity: 0
-              }
-          );
-  
-          // Log the result of adding the stock to the portfolio
-          console.log("Stock added to Portfolio:", addToPortfolio.data);
-      } catch (error) {
-          // Log the error if adding the stock to the portfolio fails
-          console.error("Error adding stock to portfolio:", error);
+    try {
+      // Attempt to add the stock
+      const addStock = await axios.post(
+        "http://localhost:5000/api/addStock",
+        {
+          artistName: artistName,
+          artistImage: artistImage,
+          spotifyId: spotifyId,
 
-          // Handle error
+          // You can define cost to be a call to the algorithm
+        }
+      );
+
+      // Log the result of adding the stock
+      console.log("Stock added to DB:", addStock.data);
+
+      // Extract the newly added stock ID from the response
+      newStockId = addStock.data.stock?._id;
+
+      if (!newStockId) {
+        throw new Error("Failed to retrieve newly added stock ID");
       }
-  
-      try {
-          // Proceed with adding the transaction
-          const response = await axios.post(
-              "http://localhost:5000/api/addTransactionToPortfolio",
-              {
-                  userId: userId,
-                  stockId: stockId,
-                  transactionType: "buy",
-                  quantity: quantity
-              }
-          );
-  
-          console.log("Buy successful:", response.data);
-          closeModal();
-          alert(`You have successfully bought ${quantity} shares of ${artistName}!`);
-      } catch (error) {
-          // Log the error if adding the transaction fails
-          console.log({
-            userId: userId,
-            stockId: stockId,
-            transactionType: "buy",
-            quantity: quantity
-        })
-          console.error("Error buying:", error);
-          // Handle error
-          return;
+
+      // Update stockId with newStockId
+      stockId = newStockId;
+    } catch (error) {
+      // Log the error if adding the stock fails
+      console.error("Error adding stock:", error);
+    }
+
+    try {
+      // Add the newly added stock to the user's portfolio
+      const addToPortfolio = await axios.post(
+        "http://localhost:5000/api/addStockToPortfolio",
+        {
+          userId: userId,
+          stockId: stockId,
+          quantity: 0
+        }
+      );
+
+      // Log the result of adding the stock to the portfolio
+      console.log("Stock added to Portfolio:", addToPortfolio.data);
+    } catch (error) {
+      // Log the error if adding the stock to the portfolio fails
+      console.error("Error adding stock to portfolio:", error);
+
+      // Handle error
+    }
+
+    try {
+      // Proceed with adding the transaction
+      const response = await axios.post(
+        "http://localhost:5000/api/addTransactionToPortfolio",
+        {
+          userId: userId,
+          stockId: stockId,
+          transactionType: "buy",
+          quantity: quantity
+        }
+      );
+
+      console.log("Buy successful:", response.data);
+      closeModal();
+      alert(`You have successfully bought ${quantity} shares of ${artistName}!`);
+    } catch (error) {
+      // Log the error if adding the transaction fails
+      console.error("Error buying:", error);
+
+      // Handle error
+      if (error.response.data.status === 406) {
+        alert("Insufficient Stock");
+      } else if (error.response.data.status === 402) {
+        alert("Insufficient Funds");
+      } else {
+        // Handle other errors
+        console.error("Unhandled error:", error);
       }
+
+      closeModal();
+    }
+
   };
-  
-    
-    
+
+
+
   return (
     <div className={showModal ? "block" : "hidden"}>
       <div className="fixed z-10 inset-0 overflow-y-auto">
@@ -117,22 +122,22 @@ const BuyModal = ({ showModal, closeModal, userId, stockId, artistImage, artistN
               </div>
             </div>
             <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <button
-                  type="button"
-                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
-                  onClick={() => handleBuy(userId, stockId, artistImage, artistName, quantity, spotifyId)}
-                >
-                  Buy
-                </button>
+              <button
+                type="button"
+                className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
+                onClick={() => handleBuy(userId, stockId, artistImage, artistName, quantity, spotifyId)}
+              >
+                Buy
+              </button>
 
-                <button
-                  type="button"
-                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                  onClick={closeModal}
-                >
-                  Cancel
-                </button>
-              </div>
+              <button
+                type="button"
+                className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                onClick={closeModal}
+              >
+                Cancel
+              </button>
+            </div>
           </div>
         </div>
       </div>
