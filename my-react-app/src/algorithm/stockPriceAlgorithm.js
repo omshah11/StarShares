3f05666b2c1dcee91202f800751b9840ff4e3628
andyleezhou <andy.lee.zhou@gmail.com>
2024-05-08 12:41:55 -0700

    changed default transactionCount to 5, this will scale down price by * 0.15 for any artists under 5 transactions. Greater than around 30 is when the scaling will become positive

--- my-react-app/src/algorithm/stockPriceAlgorithm.js
+++ my-react-app/src/algorithm/stockPriceAlgorithm.js
@@ -4,15 +4,15 @@ const stockPriceAlgorithm = (popularity, followers, transactionCount) => {
   const FOLLOWERS_WEIGHT = 0.3;
   const TRADE_WEIGHT = 0.3;
 
-  if (transactionCount === 0) {
-    transactionCount = 1;
+  if (transactionCount < 5) {
+    transactionCount = 5;
   }
 
   // Calculate valuation using weighted sum
   let valuation =
     ((POPULARITY_WEIGHT * popularity) / 100 + // 0.4 * 100 / 100 ==> 40
     (FOLLOWERS_WEIGHT * followers) / 1000 ) * // 0.3 * 20,000,000 / 1000 ==> 7,000
-    (TRADE_WEIGHT * ( transactionCount /10 )); // 0.3 * (50 / 10) ==> 1.33
+    (TRADE_WEIGHT * ( transactionCount /10 )); // 0.3 * (5 / 10) ==> 0.15
   valuation = Number(valuation.toFixed(2));
   return valuation;
 };
